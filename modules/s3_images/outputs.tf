f681251cb6cd6ee16499fab9b01aa3d4aca55b37
oalexyeyev <alexyeyev@gmail.com>
2025-12-01 17:34:28 +0200

    CORE-16: update README and Terraform files for improved clarity and consistency

--- modules/s3_images/outputs.tf
+++ modules/s3_images/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
-  value = aws_s3_bucket.images.bucket
+  value       = aws_s3_bucket.images.bucket
   description = "Name of the S3 bucket for storing images"
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.images.arn
+  value       = aws_s3_bucket.images.arn
   description = "ARN of the S3 bucket for storing images"
 }
